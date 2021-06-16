5e38f0ccf71f8e9bcc9b57869f8bb54a5ce94ede
alaa <adeenib@gmail.com>
2021-06-16 18:29:25 +0300

    my nose

---
+++ JavaSpring/TheCode/src/main/java/com/thecode/homePage.java
@@ -0,0 +1,31 @@
+package com.thecode;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class homePage {
+	@RequestMapping("")
+	public String home() {
+		return "home.jsp";
+	}
+	@RequestMapping("/createError")
+	public String flashMessages(RedirectAttributes redirectAttributes) {
+	        redirectAttributes.addFlashAttribute("error", "You must train hard!");
+	        return "redirect:/";
+	    }
+	@RequestMapping(value="/code", method=RequestMethod.POST)
+	public String codePage(@RequestParam(value="thecode")String thecode) {
+		if(thecode.equals("bushido")) {
+			return "secrat.jsp";
+		}
+		
+		return "redirect:/createError";
+			
+		
+		
+	}
+}
